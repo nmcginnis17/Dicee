8556d7cf6fa968ee12e4875c390cc1d334727997
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2020-03-27 21:40:48 -0400

    updated for iOS13

--- ViewController.swift
+++ ViewController.swift
@@ -1,53 +1,55 @@
 //
 //  ViewController.swift
-//  Dicee
+//  Dicee-iOS13
 //
-//  Created by Nicholas McGinnis on 12/4/17.
-//  Copyright © 2017 Sinnig Media. All rights reserved.
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    var randomDiceIndex1 : Int = 0
-    var randomDiceIndex2 : Int = 0
-
-    @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    var diceTotalValue: Int = 0
+    
     
 //    Array for dice images
-    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    let diceArray = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+
+//    Dice image views
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceRollTotalLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateDiceImages()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-//    Roll Button Function
+    
+//    Update dice images when roll button is pressed
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         updateDiceImages()
+        updateTotalLabel()
     }
     
-    
     func updateDiceImages() {
-//        Generate random number 1 through 6
+//        Generate random dice numbers
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
-        
-//        Assign random generated number to die, then display correct image
+                
+//        Assign dice number to image view
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
-    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-        updateDiceImages()
+    func updateTotalLabel() {
+        diceTotalValue = ((randomDiceIndex1 + 1) + (randomDiceIndex2 + 1))
+        
+        diceRollTotalLabel.text = "You rolled a \(diceTotalValue)"
     }
-    
+
+
 }
 
